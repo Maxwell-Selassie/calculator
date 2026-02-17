e5af8714276590214ba1348e0f2020be18d7ee65
Maxwell Selassie <maxwellselassie2004@gmail.com>
2026-02-17 11:17:05 +0000

    Rust Tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-// fn main() {
-//     let word = "42";
-//     let number: u32 = word.trim().parse().expect("Input a number");
+struct Contact {
+    name : String,
+    phone : String,
+    age : i32,
+}
 
-//     println!("Number multiplied by 2 is {}", &number * 2)
-// }
+impl Contact {
+    fn is_adult(&self) -> bool {
+        self.age > 18
+    }
+}
+fn main() {
+    let contact_card = Contact {
+        name : String::from("Maxwell Selassie Hiamatsu"),
+        phone : String::from("+1 23 45 654 423"),
+        age : 34,
+    };
 
+    println!("Contact Name : {}",contact_card.name);
+    println!("Phone number : {}", contact_card.phone);
+    println!("Age : {}",contact_card.age);
+    
+    
+    if contact_card.is_adult() {
+        println!("You are above 18 years old. Access granted");
+    }
+    else {
+        println!("You are less than 18 years old. Access denied");
+    }
+}
