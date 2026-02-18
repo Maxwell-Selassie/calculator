d9d44a8730be12e0a1e6e08c4a4a30703d501ba4
Maxwell Selassie <maxwellselassie2004@gmail.com>
2026-02-18 15:06:03 +0000

    Rust tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,60 @@
-struct Contact {
-    name : String,
-    phone : String,
-    age : i32,
-}
+// struct Contact {
+//     name : String,
+//     phone : String,
+//     age : i32,
+// }
 
-impl Contact {
-    fn is_adult(&self) -> bool {
-        self.age > 18
-    }
-}
-fn main() {
-    let contact_card = Contact {
-        name : String::from("Maxwell Selassie Hiamatsu"),
-        phone : String::from("+1 23 45 654 423"),
-        age : 34,
-    };
+// impl Contact {
+//     fn is_adult(&self) -> bool {
+//         self.age > 18
+//     }
+// }
+// fn main() {
+//     let contact_card = Contact {
+//         name : String::from("Maxwell Selassie Hiamatsu"),
+//         phone : String::from("+1 23 45 654 423"),
+//         age : 34,
+//     };
 
-    println!("Contact Name : {}",contact_card.name);
-    println!("Phone number : {}", contact_card.phone);
-    println!("Age : {}",contact_card.age);
+//     println!("Contact Name : {}",contact_card.name);
+//     println!("Phone number : {}", contact_card.phone);
+//     println!("Age : {}",contact_card.age);
     
     
-    if contact_card.is_adult() {
-        println!("You are above 18 years old. Access granted");
+//     if contact_card.is_adult() {
+//         println!("You are above 18 years old. Access granted");
+//     }
+//     else {
+//         println!("You are less than 18 years old. Access denied");
+//     }
+// }
+
+use std::io;
+
+struct Task {
+    title: String,
+    completed: bool,
+}
+
+impl Task {
+    fn new(title: String) -> Self {
+        Self {
+            title, 
+            completed: false
+        }
     }
-    else {
-        println!("You are less than 18 years old. Access denied");
+
+    fn mark_done(&mut self) {
+        self.completed = true
+    }
+
+    fn display(&self, index: usize) {
+        let status = if self.completed {"Done"} else {""};
+        println!("{}, {} [{}]", index + 1, self.title, status);
     }
 }
+
+struct TaskList {
+    tasks:vec<Task>
+}
+
